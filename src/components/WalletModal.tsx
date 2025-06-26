4190a917d98f0d2966718638f83c1bfa53db8f2c
Freddie philpot <pphilfre@monkswalk.herts.sch.uk>
2025-06-26 10:21:19 +0100

    Updated WalletModal.tsx

--- src/components/WalletModal.tsx
+++ src/components/WalletModal.tsx
@@ -169,7 +169,7 @@ export const WalletModal: React.FC<WalletModalProps> = ({ isOpen, onClose }) => 
 
             <div>
               <label className="block text-sm font-medium text-gray-300 mb-2">
-                Amount (Demo)
+                Amount
               </label>
               <input
                 type="number"
