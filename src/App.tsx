522ba719d2a30a724a1473d0c7e5346fd43f8d36
Freddie philpot <pphilfre@monkswalk.herts.sch.uk>
2025-06-28 20:42:43 +0100

    Updated package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Header } from './components/layout/Header'
-import { Hero } from './components/Hero'
-import { Casino } from './components/Casino'
-import { WalletProvider } from './contexts/WalletContext'
-import { GameProvider } from './contexts/GameContext'
-import { AdminProvider } from './contexts/AdminContext'
-import { AuthProvider } from './contexts/AuthContext'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Hero } from './components/Hero';
+import { Casino } from './components/Casino';
+import { Sportsbook } from './components/SportsbookNew';
+import { WalletProvider } from './contexts/WalletContext';
+import { GameProvider } from './contexts/GameContext';
+import { AdminProvider } from './contexts/AdminContext';
+import { SportsProvider } from './contexts/SportsContext';
 
 function App() {
   return (
-    <AuthProvider>
-      <WalletProvider>
-        <GameProvider>
-          <AdminProvider>
+    <WalletProvider>
+      <GameProvider>
+        <AdminProvider>
+          <SportsProvider>
             <Router>
-              <div className="min-h-screen bg-[#0f1419]">
+              <div className="min-h-screen bg-gradient-to-br from-slate-900 via-slate-800 to-blue-900">
                 <Header />
                 <Routes>
                   <Route path="/" element={<Hero />} />
                   <Route path="/casino" element={<Casino />} />
+                  <Route path="/sportsbook" element={<Sportsbook />} />
                 </Routes>
               </div>
             </Router>
-          </AdminProvider>
-        </GameProvider>
-      </WalletProvider>
-    </AuthProvider>
-  )
+          </SportsProvider>
+        </AdminProvider>
+      </GameProvider>
+    </WalletProvider>
+  );
 }
 
-export default App
+export default App;
